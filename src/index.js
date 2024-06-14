2fea10a9ab121489015cc00949fc151cd3d07782
massoncorlette <massoncorlette07@gmail.com>
2024-06-14 01:28:07 -0400

    have the general layout before starting DOM and logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 //index is entry point for application 
 // import modules, initialize app
 import './style.css';
+import sidebarIcon from './icons/sidebar.svg';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebarBtn = document.getElementById('sidebar-icon');
+  const btnElement = document.createElement('button');
+  const btnSvg = document.createElement('img');
+
+  btnSvg.src = sidebarIcon;
+  btnElement.appendChild(btnSvg);
+  sidebarBtn.appendChild(btnElement);
+
+});
 
 console.log("test");
